51940ee06ae9332572622445207313b6ac23598f
ztach <ztach@poczta.onet.pl>
2018-05-08 18:57:53 +0200

    śledzenie routingu

---
+++ projekt2/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projekt2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home ActionResult 
+        public string Index()
+        {
+            string wynik = "";
+            foreach (string routkey in RouteData.Values.Keys)
+            {
+                wynik += "klucz: " + routkey + " wartość: " + RouteData.Values[routkey].ToString();
+                wynik += "<br />";
+            }
+
+            return wynik;
+           // return "witaj Stachu";// View();
+        }
+    }
+}
