906a456e292aada7b84aa1bd126ed492315f9cf8
ztach <ztach@poczta.onet.pl>
2018-05-11 09:50:35 +0200

    zbieram_strony_widoki

---
+++ projekt4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="projekt4.MvcApplication" Language="C#" %>
---
+++ projekt4/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace projekt4.Models
+{
+    public class Person
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public int Age { get; set; }
+        /*
+        public Person(string sImie, string sNazwisko, int iAge)
+        {
+            Imie = sImie;
+            Nazwisko = sNazwisko;
+            Age = iAge;
+        }
+        */
+
+    }
+}
