1699d89bcb4cd214f5089fdef8b9ed292ff4ed65
ztach <ztach@poczta.onet.pl>
2018-05-16 12:03:47 +0200

    model - klasa Pracownik

---
+++ Walidacja/Models/Pracownik.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Walidacja.Models
+{
+    public class Pracownik
+    {
+        public int PracownikId { get; set; }
+        public string Imie { get; set; }
+        public int Wiek { get; set; }
+        public string Email { get; set; }
+        public string NumerTelefonu { get; set; }
+        public string Pytanie { get; set; }
+
+    }
+}
