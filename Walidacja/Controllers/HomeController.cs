b2fd1725f7d0ffbfff78f90dc038529847e89990
ztach <ztach@poczta.onet.pl>
2018-05-16 12:06:21 +0200

    kontroler Home i index(pracownik)

---
+++ Walidacja/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Walidacja.Models;
+
+namespace Walidacja.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Pracownik pracownik)
+        {
+            return View();
+        }
+    }
+}
